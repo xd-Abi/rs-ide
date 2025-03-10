d05b6cc720444994581482efb46edb66a6147f22
Abinayan Sureskumar <abi@tie-international.com>
2025-03-10 14:40:29 +0100

    Creating a window using tao

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use platform::{Window, WindowTrait};
-
-mod platform;
+use tao::{
+    event::{ElementState, Event, MouseButton, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
-    let window = Window::new("Rust IDE", 800, 600);
+    env_logger::init();
+    let event_loop = EventLoop::new();
 
-    loop {
-        window.update();
-    }
+    let window = WindowBuilder::new()
+        .with_title("Rust IDE")
+        .with_decorations(false)
+        .build(&event_loop).expect("Failed to create window");
+
+    let mut mouse_y_pos = 0.0;
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event, ..
+        } => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::MouseInput {
+                state: ElementState::Pressed,
+                button: MouseButton::Left,
+                ..
+            } => {
+                // Title bar height
+                if mouse_y_pos < 55.0 {
+                    window.drag_window().expect("Failed to drag window")
+                }
+            },
+            WindowEvent::CursorMoved { position, .. } => {
+                mouse_y_pos = position.y;
+            }
+            _ => (),
+        },
+        _ => (),
+    });
 }
