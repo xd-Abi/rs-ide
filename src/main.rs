3ec1806a5a7f9a7e0b2c393144cebe302ac3d816
Abinayan Sureskumar <abi@tie-international.com>
2025-03-10 17:12:55 +0100

    Working with winit instead of tao

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,106 @@
-use tao::{
-    event::{ElementState, Event, MouseButton, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use log::{debug, info};
+use std::collections::HashMap;
+use std::ptr::addr_eq;
+use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalPosition;
+use winit::error::ExternalError;
+use winit::event::{ElementState, MouseButton, WindowEvent};
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{ResizeDirection, Window, WindowId};
+
+#[derive(Default)]
+struct App {
+    windows: HashMap<WindowId, Window>,
+    mouse: Option<PhysicalPosition<f64>>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        info!("App Resumed!");
+        let window_attributes = winit::window::WindowAttributes::default()
+            .with_title("My Window")
+            .with_decorations(false)
+            .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0));
+
+        let window = event_loop
+            .create_window(window_attributes)
+            .expect("Failed to create window");
+        self.windows.insert(window.id(), window);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                info!("Window {:?} closed", window_id);
+                event_loop.exit();
+            }
+            WindowEvent::Resized(physical_size) => {
+                info!("Window {:?} resized to: {:?}", window_id, physical_size);
+            }
+            WindowEvent::MouseInput { state, button, .. } => {
+                if state == ElementState::Pressed {
+                    if let Some(position) = self.mouse {
+                        if let Some(window) = self.windows.get(&window_id) {
+
+                            let size = window.inner_size();
+                            let border_threshold = 20.0;
+                            let title_bar_thickness = 55.0;
+
+                            // @TODO: Corners
+                            if position.x < border_threshold {
+                                window
+                                    .drag_resize_window(ResizeDirection::West)
+                                    .expect("Failed to resize window");
+                            } else if position.x > size.width as f64 - border_threshold {
+                                window
+                                    .drag_resize_window(ResizeDirection::East)
+                                    .expect("Failed to resize window");
+                            } else if position.y < border_threshold {
+                                window
+                                    .drag_resize_window(ResizeDirection::North)
+                                    .expect("Failed to resize window");
+                            } else if position.y > size.height as f64 - border_threshold {
+                                window
+                                    .drag_resize_window(ResizeDirection::South)
+                                    .expect("Failed to resize window");
+                            } else if position.y < title_bar_thickness {
+                                window.drag_window().expect("Failed to grab drag window");
+                            }
+                        }
+                    }
+                }
+            }
+            WindowEvent::CursorMoved { position, .. } => {
+                self.mouse = Some(position);
+            }
+            _ => {}
+        }
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        //info!("Event loop about to wait!");
+    }
+
+    fn suspended(&mut self, event_loop: &ActiveEventLoop) {
+        info!("App suspended!");
+    }
+
+    fn exiting(&mut self, event_loop: &ActiveEventLoop) {
+        info!("App is exiting!");
+    }
+}
 
 fn main() {
     env_logger::init();
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoop::new().expect("Failed to create event loop");
+    let mut app = App::default();
 
-    let window = WindowBuilder::new()
-        .with_title("Rust IDE")
-        .with_decorations(false)
-        .build(&event_loop).expect("Failed to create window");
-
-    let mut mouse_y_pos = 0.0;
-
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            event, ..
-        } => match event {
-            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-            WindowEvent::MouseInput {
-                state: ElementState::Pressed,
-                button: MouseButton::Left,
-                ..
-            } => {
-                // Title bar height
-                if mouse_y_pos < 55.0 {
-                    window.drag_window().expect("Failed to drag window")
-                }
-            },
-            WindowEvent::CursorMoved { position, .. } => {
-                mouse_y_pos = position.y;
-            }
-            _ => (),
-        },
-        _ => (),
-    });
+    event_loop
+        .run_app(&mut app)
+        .expect("Failed to run event loop");
 }
